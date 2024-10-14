1077851df35936c3a6e14ec78269bbecb9948820
Vaněk Tomáš <L20220058@SSPTAJI.CZ>
2024-10-14 12:55:43 +0200

    tlak

--- app.js
+++ app.js
@@ -9,3 +9,59 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+function spocitat(){
+    var hodnota_a = parseInt(document.getElementById('a').value);
+    var hodnota_b = parseInt(document.getElementById('b').value);
+    var hodnota_c = parseInt(document.getElementById('c').value);
+
+    let vysledek;
+
+    if (isNaN(hodnota_a) && isNaN(hodnota_b)){
+        document.getElementById('text').innerText = "Zadej dvě hodnoty!";
+    }
+    else{
+        if (isNaN(hodnota_b) && isNaN(hodnota_c)){
+            document.getElementById('text').innerText = "Zadej dvě hodnoty!";
+        }
+        else{
+            if (isNaN(hodnota_c) && isNaN(hodnota_a)){
+                document.getElementById('text').innerText = "Zadej dvě hodnoty!";
+            }
+            else{
+                if (isNaN(hodnota_a)){
+                    vysledek = hodnota_b * hodnota_c;
+                    document.getElementById('a').innerText = vysledek;
+                    spocitat();
+                }
+                else{
+                    if (isNaN(hodnota_b)){
+                        if (hodnota_c === 0){
+                            document.getElementById('text').innerText = "Dělení nulou";
+                            spocitat();
+                        }
+                        else{
+                            vysledek = hodnota_a / hodnota_c;
+                            document.getElementById('b').innerText = vysledek;
+                        }
+                        
+                    }
+                    else{
+                        if (isNaN(hodnota_c)){
+                            if (hodnota_b === 0){
+                                document.getElementById('text').innerText = "Dělení nulou";
+                                spocitat();
+                            }
+                            else{
+                                vysledek = hodnota_a / hodnota_b;
+                                document.getElementById('c').innerText = vysledek;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+   
+    
+}
